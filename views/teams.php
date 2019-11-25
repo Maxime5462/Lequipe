293f7a82adf6bc5dcbc83781c028c6a71dda64d2
Maxime <maxime.leszczynski@laposte.net>
2019-11-25 15:57:58 +0100

    Commit3

--- views/teams.php
+++ views/teams.php
@@ -1,26 +1,30 @@
 <?php $title = "Teams"; ?>
 <?php ob_start(); ?>
 
-<div class="container">
 
-    <?php foreach ($listTeam as $team) { ?>
-        <div class="row">
 
-                <img src="<?php echo $team['logo']; ?>" alt="Logo " >
+<?php foreach ($listTeam as $team) { ?>
+    <div class="col-3">
 
-                <p>Nom :<?php echo $team['name']; ?></p>
-                <p>Nom raccourci :<?php echo $team['short_name']; ?></p>
-                <p>Fondé le :<?php echo $team['fundation_date']; ?></p>
-                <p>Président :<?php echo $team['president']; ?></p>
-                <p>Adresse :<?php echo $team['adress']; ?></p>
-                <a href="<?php echo $team['website']; ?>">Lien</a>
-                <p>Site web :<?php echo $team['website']; ?></p>
-                <p>Lien :<?php echo $team['link']; ?></p>
 
+        <div class="card mt-3 ml-3 " style="width: 18rem;">
+            <img class="card-img-top" src="<?php echo $team['logo']; ?>" alt="Card image cap">
+            <div class="card-body">
+                <p class="card-title ">Nom :<?php echo $team['name']; ?></p>
+                <p class="card-text ">
+                    <p>Nom raccourci :<?php echo $team['short_name']; ?></p>
+                    <p>Fondé le :<?php echo $team['fundation_date']; ?></p>
+                    <p>Président :<?php echo $team['president']; ?></p>
+                    <p>Adresse :<?php echo $team['adress']; ?></p>
+                    <p>Site web :<?php echo $team['website']; ?></p>
+                    <p>Lien :<?php echo $team['link']; ?></p></p>
+                    <a href="<?php echo $team['website']; ?>" class="btn btn-primary">Lien</a>
+                </div>
+            </div>
         </div>
     <?php } ?>
 
-</div>
 
-<?php $content = ob_get_clean(); ?>
-<?php require('public/index.php'); ?>
+
+    <?php $content = ob_get_clean(); ?>
+    <?php require('public/index.php'); ?>
